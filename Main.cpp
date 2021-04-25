2e0066056323f26620d34b75b0588a20ac705ff5
AylinRojasOrrego <aylin.rojas@sansano.usm.cl>
2021-04-25 16:35:33 -0400

    Main c++

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std; 
+
+string linea;
+
+int main(){
+    ifstream archivo("input1.txt");
+
+
+    system("pause");
+}
